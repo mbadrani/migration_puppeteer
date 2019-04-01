d02575c8fe1b2f9c4e1552f83d7415a315be62ba
Mehdi Badrani <mehdi.badrani@prestashop.com>
2019-04-01 17:50:46 +0200

    UIMap Mass Migration Selectors: - BO/international/index.js - BO/international/localization.js

--- tests/E2E/test/selectors/BO/design/theme_catalog.js
+++ tests/E2E/test/selectors/BO/design/theme_catalog.js
@@ -2,7 +2,7 @@ module.exports = {
   ThemeCatalog: {
     discover_all_of_the_theme_button: '#content" | #main-div  a [contains(text(), "Discover all of the themes")]',
     category_name_text: '#category_name',
-    discover_button: '#content" | #main-div"] a :nth-child(1) p[contains(text(), "Discover")]):nth-child(%POS)',
+    discover_button: '#content" | #main-div"] a:nth-child(1) p[contains(text(), "Discover")]):nth-child(%POS)',
     theme_name: '#content" | #main-div a:nth-child(1) p[class="bold"]:nth-child(%POS)',
     search_addons_input: '#addons-search-box',
 
