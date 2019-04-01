deea768a9a081fcdfad592373daedc30e7ba00d6
Mehdi Badrani <mehdi.badrani@prestashop.com>
2019-04-01 20:41:33 +0200

    UIMap Mass Migration Selectors: - BO/international/taxes.js - BO/international/translations.js

--- tests/E2E/test/selectors/BO/international/translations.js
+++ tests/E2E/test/selectors/BO/international/translations.js
@@ -1,15 +1,15 @@
 module.exports = {
   Translations:{
-    translations_menu:'//*[@id="subtab-AdminTranslations"]/a',
-    international_menu:'//*[@id="subtab-AdminInternational"]/a',
-    translations_type:'//*[@id="form_modify_translations_translation_type"]',
-    translations_language:'//*[@id="form_modify_translations_language"]',
-    modify_button:'//*[@id="main-div"]//form[@name="modify_translations"]//button[@class="btn btn-primary"]/span',
-    shop_button:'(//div[@label="Shop"])[31]',
-    theme_button:'//div[@label="Theme"]',
-    action_button:'//div[@label="Actions"]',
-    Sign_out_textarea_button:'//*[@id="app"]/div[2]/div[2]/div[2]/div/div/form/div[11]/textarea',
-    save_button:'//*[@id="app"]/div[2]/div[2]/div[2]/div/div/form/div[22]/div/button',
-    sign_out_FO_text:'//*[@id="_desktop_user_info"]//a[contains(@class,"logout")]',
+    translations_menu:'#subtab-AdminTranslations a',
+    international_menu:'#subtab-AdminInternationala',
+    translations_type:'#form_modify_translations_translation_type',
+    translations_language:'#form_modify_translations_language"]',
+    modify_button:'#main-div form[name="modify_translations"] button.btn btn-primary span',
+    shop_button:' div[label="Shop"]:nth-child(31)',
+    theme_button:' div[label="Theme"]',
+    action_button:' div[label="Actions"]',
+    Sign_out_textarea_button:'#app div:nth-child(2) div:nth-child(2) div:nth-child(2) div div form div:nth-child(11) textarea',
+    save_button:'#app div:nth-child(2) div:nth-child(2) div:nth-child(2) div div form div:nth-child(22) div button',
+    sign_out_FO_text:'#_desktop_user_info a.logout',
   }
 };
