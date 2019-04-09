ada16d6f1af5c61107bf95052310b767d2f06aa5
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-04-09 18:44:13 +0200

    Migration of test Catalogbulkactions

--- tests/E2E/test/clients/catalogbulkaction.js
+++ tests/E2E/test/clients/catalogbulkaction.js
@@ -3,23 +3,21 @@ var Product = require('./product/product');
 
 class Catalog extends Product {
 
-  selectAllProducts(selector) {
-    return this.client
-      .refresh()
-      .waitForExistAndClick(selector);
+  async selectAllProducts(selector) {
+    await page.$eval(selector, (selector) => {
+      selector.click();
+    });
   }
 
-  selectAction(CatalogPage, action) {
-    return this.client
-      .waitForExistAndClick(CatalogPage.action_group_button)
-      .waitForExistAndClick(CatalogPage.action_button.replace('%ID', action));
+  async selectAction(CatalogPage, action) {
+    await this.waitForExistAndClick(CatalogPage.action_group_button)
+    await this.waitForExistAndClick(CatalogPage.action_button.replace('%ID', action));
   }
 
   checkExistenceProduct(selector, textToCheckWith, pause = 0) {
-    return this.client
-      .pause(pause)
-      .waitForExist(selector, 9000)
-      .then(() => this.client.getText(selector))
+    return page
+      .waitForSelector(selector)
+      .then(() => this.getText(selector))
       .then((text) => expect(text.toLowerCase()).to.equal(textToCheckWith.toLowerCase()));
   }
 }
